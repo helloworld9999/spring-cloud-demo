7dc14e996e1219a5fdcc80a632bf412399661e1d
xutong <asdaa123@126.com>
2018-01-07 14:27:14 +0800

    配置文件

--- config-server/src/main/java/com/xutong/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/xutong/configserver/ConfigServerApplication.java
@@ -2,10 +2,11 @@ package com.xutong.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringCloudApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
