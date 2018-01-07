23bf35c3292230785a257acadd7a7ea2fbfcbb78
xutong <asdaa123@126.com>
2018-01-07 14:10:05 +0800

    配置文件

--- api-gateway/src/main/java/com/xutong/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/xutong/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,12 @@ package com.xutong.apigateway;
 
 import com.xutong.apigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
-@SpringBootApplication
+@SpringCloudApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
