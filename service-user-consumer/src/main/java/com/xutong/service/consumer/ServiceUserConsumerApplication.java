053d27aecc6f61fbf2d6b985f8b67d10741f680e
xutong <asdaa123@126.com>
2018-01-07 10:47:43 +0800

    添加仓库

---
+++ service-user-consumer/src/main/java/com/xutong/service/consumer/ServiceUserConsumerApplication.java
@@ -0,0 +1,20 @@
+package com.xutong.service.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceUserConsumerApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceUserConsumerApplication.class, args);
+	}
+}
---
+++ service-user-consumer/src/test/java/com/xutong/service/consumer/ServiceUserConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xutong.service.consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceUserConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
