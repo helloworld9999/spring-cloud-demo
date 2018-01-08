48f74526263d1cb8fb41fb02ca0e8c4c74c7d1d4
xutong <asdaa123@126.com>
2018-01-08 21:07:14 +0800

    添加spring-boot-admin监控

--- service-user-consumer/src/main/java/com/xutong/service/consumer/ServiceUserConsumerApplication.java
+++ service-user-consumer/src/main/java/com/xutong/service/consumer/ServiceUserConsumerApplication.java
@@ -2,15 +2,17 @@ package com.xutong.service.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableFeignClients
 @EnableDiscoveryClient
-@SpringBootApplication
+@EnableHystrix
+@EnableCircuitBreaker
+@SpringCloudApplication
 public class ServiceUserConsumerApplication {
 
 
--- service-user-consumer/src/main/java/com/xutong/service/consumer/controller/userConsumerController.java
+++ service-user-consumer/src/main/java/com/xutong/service/consumer/controller/userConsumerController.java
@@ -1,5 +1,6 @@
 package com.xutong.service.consumer.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.xutong.service.consumer.api.ConsumerUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ public class userConsumerController {
     @Autowired
     private ConsumerUserService userservice;
 
+    @HystrixCommand
     @RequestMapping(value = "/consumersayHello",method = RequestMethod.GET)
     public String sayHello(String name){
         return userservice.sayHello(name);
